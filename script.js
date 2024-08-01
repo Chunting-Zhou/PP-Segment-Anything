0f77a5b7e23aa1607e92ab71d12cdff75ceadb78
Sins <2309484292@qq.com>
2024-08-01 17:36:13 +0800

    Updated website structure and styles

--- script.js
+++ script.js
@@ -1,35 +1,26 @@
 let slideIndex = 0;
 showSlides(slideIndex);
 
-function plusSlides(n) {
+function changeSlide(n) {
     showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("slide");
+    let dots = document.getElementsByClassName("dot");
     if (n >= slides.length) { slideIndex = 0 }
     if (n < 0) { slideIndex = slides.length - 1 }
-
-    console.log('Total slides:', slides.length);
-    console.log('Current slide index:', slideIndex);
-
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
-        console.log('Slide', i, 'set to display:none');
     }
-    slides[slideIndex].style.display = "flex";
-    console.log('Slide', slideIndex, 'set to display:flex');
-
-    const currentSlide = slides[slideIndex];
-    const imageRows = currentSlide.getElementsByClassName('image-row');
-    console.log('Number of image rows in current slide:', imageRows.length);
-
-    for (i = 0; i < imageRows.length; i++) {
-        const images = imageRows[i].getElementsByTagName('img');
-        console.log('Number of images in row', i, ':', images.length);
-        for (let j = 0; j < images.length; j++) {
-            console.log('Image', j, 'src:', images[j].src);
-        }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
     }
+    slides[slideIndex].style.display = "block";
+    dots[slideIndex].className += " active";
 }
